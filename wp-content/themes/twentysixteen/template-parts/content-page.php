8c812166d802b7f71235face0bf9706432be4180
joshi <jkoyilerian@solvd.com>
2026-03-25 17:43:52 +0530

    testing for pant test envt 2

--- wp-content/themes/twentysixteen/template-parts/content-page.php
+++ wp-content/themes/twentysixteen/template-parts/content-page.php
@@ -10,7 +10,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?> Test envt
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?> Test envt 2
 	</header><!-- .entry-header -->
 
 	<?php twentysixteen_post_thumbnail(); ?>
